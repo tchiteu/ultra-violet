028713c7614620ed3fb5b49056078f027c9fdbf6
tchiteu <matheussantos.contato362@gmail.com>
2023-01-31 21:06:22 -0300

    feat(theme): initializing theme defaults :art:

---
+++ src/theme/theme.ts
@@ -0,0 +1,18 @@
+const colors = {
+  primary: '#1E5EFF',
+  secondary: '#3DFC7C',
+  background: '#212429',
+  background_secondary: '#323741',
+  text_primary: '#FEFEFE',
+  text_secondary: '#EDEDEE',
+};
+
+const fontSizes = {
+  xs: 8,
+  sm: 12,
+  md: 16,
+  lg: 20,
+  xl: 24,
+};
+
+export { colors, fontSizes };
