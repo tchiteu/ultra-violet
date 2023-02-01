40fb22056f36aa01f78900eaecab2bebfb19ef94
tchiteu <matheussantos.contato362@gmail.com>
2023-01-31 21:07:36 -0300

    feat(screen): creating Home screen :building_construction:

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UltraTemplate from '@components/UltraTemplate';
+import UltraButton from '@components/UltraButton';
+
+const Home: React.FC = () => {
+  return (
+    <UltraTemplate>
+      <UltraButton>LET'S GO!</UltraButton>
+    </UltraTemplate>
+  );
+};
+
+export default Home;
