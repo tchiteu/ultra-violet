a7b3046750999204a427bfac69cc67358ab898b9
tchiteu <matheussantos.contato362@gmail.com>
2023-02-02 20:00:09 -0300

    feat(theme): adding new colors to theme :art:

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,9 +2,12 @@ const colors = {
   primary: '#1E5EFF',
   secondary: '#3DFC7C',
   background: '#212429',
-  background_secondary: '#323741',
-  text_primary: '#FEFEFE',
-  text_secondary: '#EDEDEE',
+  background_secondary: '#292F3A',
+  text_primary: '#F7FAFE',
+  text_secondary: '#757E95',
+  light_grey: '#323741', 
+  light_blue: '#1E47AE',
+  dark_blue: '#213469',
 };
 
 const fontSizes = {
